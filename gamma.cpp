fbe2038598dae81887ee72930e80467fa15bf5aa
marshallmm <marshallpmiller@gmail.com>
2017-03-26 20:41:51 -0700

    first commit for gamma project

---
+++ gamma.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <assert.h>
+#include <string.h>
+#include <random>
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+#define LYRAND (double)rand()/RAND_MAX
+
+using namespace std;
+class armm{
+public:
+    double mu;
+    int sigma;
+    void init();
+};
+void armm::init(){
+    double mu=0;
+    sigma=0;
+}
+
+int main(){
+  
+}
