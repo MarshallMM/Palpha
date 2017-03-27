09d9c0ba64ec3bebd63e5a509a529df5c5a81a7d
marshallmm <marshallpmiller@gmail.com>
2017-03-26 22:34:02 -0700

    builds cities, and vector of random policys, with evaluate function

--- gamma.cpp
+++ gamma.cpp
@@ -23,7 +23,17 @@ void city::init(){
     double latitude=0;
     double longitude=0;
 }
-
+double evaluateFitness(vector<int> PolicyF,vector<city> cityVectorF){
+  double fitness=0;
+  for(int i=0;i<PolicyF.size()-1;i++){
+    double distance=0;
+    double dx=cityVectorF[PolicyF[i]].longitude-cityVectorF[PolicyF[i+1]].longitude;
+    double dy=cityVectorF[PolicyF[i]].latitude-cityVectorF[PolicyF[i+1]].latitude;
+    distance=sqrt(dx*dx+dy*dy);
+    fitness=fitness+distance;
+  }
+  return fitness;
+}
 int main(){
     srand(time(NULL));
 
@@ -33,6 +43,8 @@ int main(){
     int NCities;
     int NPolicies;
     int swap;
+    int pick;
+    int hold;
     double maxLongitude;
     double maxLatitude;
 
@@ -40,9 +52,6 @@ int main(){
     NPolicies=100;
     maxLatitude= 100;
     maxLongitude= 100;
-
-
-
 
     //Builds vector of cities and their position
     for(int i=0;i<NCities;i++){
@@ -53,19 +62,35 @@ int main(){
       cityVector.push_back(c);
     }
 
-    //Builds initial policy vector
+    //Builds initial policy vector with random policies
     for(int i=0;i<NCities;i++){
       xPolicy.push_back(i);
     }
     for(int i=0;i<NPolicies;i++){
-      for(int j=0;j<NCities*10;j++){
-          //mixes up xPolicy
-
-        
+      for(int j=0;j<NCities*100;j++){
+        //mixes up xPolicy
+        pick=rand()%NCities;
+        swap=rand()%NCities;
+        hold=xPolicy[pick];
+        xPolicy[pick]=xPolicy[swap];
+        xPolicy[swap]=hold;
       }
       policyVector.push_back(xPolicy);
+    }//end of building the initial vector of random policies
+
+    //checks policies dont hit the same city twice
+    for(int i=0;i<NPolicies;i++){
+      for(int j=0;j<NCities;j++){
+        for(int k=0;k<NCities;k++){
+          if(j!=k){
+            assert(policyVector[i][j]!=policyVector[i][k]);
+          }
+        }
+      }
+    }//end of check of policies not hiting same city twice
+
+    for(int i=0;i<NPolicies;i++){
+      cout<<evaluateFitness(policyVector[i],cityVector)<<endl;
     }
-
-
 
 }
